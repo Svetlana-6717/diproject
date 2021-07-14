2d832939f280793c5b0d1d8ac3306d96f2fce3cd
Svetlana <sl1248@yandex.ru>
2021-07-15 02:16:12 +0300

    added top-slider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@
 
 import togglePopup from './modules/togglePopup';
 import toggleMenu from './modules/toggleMenu';
+import slider from './modules/slider';
 
 // popup Callback
 togglePopup();
 // menu
 toggleMenu();
+// slider
+slider();
---
+++ src/modules/slider.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const slider = () => {
+  const slider = document.querySelector('.top-slider');
+  const slide = slider.querySelectorAll('.item');
+  const dot = slider.querySelectorAll('.dot');
+  const table = slider.querySelectorAll('.table');
+
+  let currentSlide = 0;
+  let interval;
+
+  slide[currentSlide].classList.add('item-active');
+  dot[currentSlide].classList.add('slick-active');
+  table[currentSlide].classList.add('active');
+
+  const prevSlide = (elem, index, strClass) => {
+    elem[index].classList.remove(strClass);
+  };
+
+  const nextSlide = (elem, index, strClass) => {
+    elem[index].classList.add(strClass);
+  };
+
+  const autoPlaySlide = () => {
+    prevSlide(slide, currentSlide, 'item-active');
+    prevSlide(dot, currentSlide, 'slick-active');
+    prevSlide(table, currentSlide, 'active');
+    currentSlide++;
+    if (currentSlide >= slide.length) {
+      currentSlide = 0;
+    }
+    nextSlide(slide, currentSlide, 'item-active');
+    nextSlide(dot, currentSlide, 'slick-active');
+    nextSlide(table, currentSlide, 'active');
+  };
+
+  const startSlide = (time = 3000) => {
+    interval = setInterval(autoPlaySlide, time);
+  };
+
+  slider.addEventListener('click', (event) => {
+    let target = event.target;
+    if (!target.matches('.dot')) {
+      return;
+    }
+
+    prevSlide(slide, currentSlide, 'item-active');
+    prevSlide(dot, currentSlide, 'slick-active');
+    prevSlide(table, currentSlide, 'active');
+
+    if (target.matches('.dot')) {
+      dot.forEach((elem, index) => {
+        if (elem === target) {
+          currentSlide = index;
+        }
+      });
+
+      nextSlide(slide, currentSlide, 'item-active');
+      nextSlide(dot, currentSlide, 'slick-active');
+      nextSlide(table, currentSlide, 'active');
+    }
+  });
+  startSlide(3000);
+
+};
+
+export default slider;
